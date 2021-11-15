ef6e2d592b641c6871c192d7cc33e5052453d8c2
Felipe Gouvea <felipegalvesouvea@gmail.com>
2021-11-15 19:19:19 -0300

    fix global css import error

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,17 @@
-import '../styles/globals.scss';
+import '~/styles/global.scss';
+import React from 'react';
+
 import type { AppProps } from 'next/app';
 
+import Navbar from '~/components/Navbar';
+
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Navbar />
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp;
