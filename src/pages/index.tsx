a414fa1eb47292fddb9754f366a18ea59762a726
Felipe Gouvea <felipegalvesouvea@gmail.com>
2021-11-15 20:10:59 -0300

    create landing

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,7 @@
 import Head from 'next/head';
 
+import Button from '~/components/Button';
 import styles from '~/styles/home.module.scss';
-
-import Navbar from '../components/Navbar';
 
 export default function Home() {
   return (
@@ -11,7 +10,23 @@ export default function Home() {
         <title>Home - Felipe Gouvea</title>
       </Head>
 
-      <h1>hello world</h1>
+      <section className={styles.landing}>
+        <main className={styles.hero}>
+          <div>
+            <img src="/images/memoji.png" alt="Felipe's memoji winking" />
+          </div>
+          <h4>Hey! I’m Felipe 👋</h4>
+          <h1>Uniting experience and passion with a single purpose</h1>
+          <p>
+            a Web and Mobile Developer and UI/UX Designer who lives in the
+            wonderful city, better known as Rio de Janeiro. Graduate in systems
+            analysis and passionate about the best technologies on the market.{' '}
+            <b>Let’s build a project together?</b>
+          </p>
+
+          <Button title="contact me" />
+        </main>
+      </section>
     </>
   );
 }
