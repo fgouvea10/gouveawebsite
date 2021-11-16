97b1a37904b44aa20294091f1122b01ca91874f7
Felipe Gouvea <felipegalvesouvea@gmail.com>
2021-11-15 21:23:34 -0300

    create social media sidebar

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,11 +4,13 @@ import React from 'react';
 import type { AppProps } from 'next/app';
 
 import Navbar from '../components/Navbar';
+import Sidebar from '~/components/Sidebar';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <Navbar />
+      <Sidebar />
       <Component {...pageProps} />
     </>
   );
