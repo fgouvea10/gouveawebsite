240a4de92afa72a9b1cd5925ced8349c07dcaada
Felipe Gouvea <felipegalvesouvea@gmail.com>
2021-11-17 21:48:23 -0300

    create footer component

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import styles from './styles.module.scss';
+
+function Footer() {
+  return (
+    <footer className={styles.container}>
+      <p>Developed with &#9829; by me</p>
+    </footer>
+  );
+}
+
+export default Footer;
