5da2c964fbd7b7af4a29075c85e1a69afc531589
Felipe Gouvea <felipegalvesouvea@gmail.com>
2022-01-28 22:13:49 -0300

    refactor: change footer component

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,13 +1,44 @@
+/* eslint-disable react/no-unescaped-entities */
 import React from 'react';
 
 import styles from './styles.module.scss';
 
-function Footer() {
+export default function Footer() {
   return (
     <footer className={styles.container}>
-      <p>Developed with &#9829; by me</p>
+      <div className={styles.flex_row}>
+        <div className={styles.col}>
+          <div>Got a project in mind?</div>
+          <div>Let's talk!</div>
+        </div>
+
+        <div className={styles.social}>
+          <ul>
+            <li>
+              <a href="#">Instagram</a>
+            </li>
+            <li>
+              <a href="#">Linkedin</a>
+            </li>
+            <li>
+              <a href="#">Behance</a>
+            </li>
+            <li>
+              <a href="#">Dribbble</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+
+      <div className={styles.footer}>
+        <div>Felipe Gouvea</div>
+        <hr />
+        <div>
+          All
+          <br /> rights <br />
+          reserved
+        </div>
+      </div>
     </footer>
   );
 }
-
-export default Footer;
