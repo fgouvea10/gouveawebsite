567bba9cb7d70cb0245d71cc45eafee77c6ec4c7
Felipe Gouvea <felipegalvesouvea@gmail.com>
2022-01-29 14:05:46 -0300

    fix: recognition of import 3

--- src/components/WorkCard/index.tsx
+++ src/components/WorkCard/index.tsx
@@ -2,23 +2,20 @@ import React from 'react';
 
 import Link from 'next/link';
 
-import { formatedFeature } from '~/utils/formatedFeature';
-
 import styles from './styles.module.scss';
 
 type WorkCardProps = {
-  id?: number;
   type: string;
   name: string;
   href: string;
 };
 
-export default function WorkCard({ id, type, name, href }: WorkCardProps) {
+export default function WorkCard({ type, name, href }: WorkCardProps) {
   return (
     <Link href={`/works/${href}`}>
       <article className={styles.container}>
         <div className={styles.footer}>
-          <div className={styles.category}>{formatedFeature(type)}</div>
+          <div className={styles.category}>{type}</div>
           <div className={styles.name}>{name}</div>
         </div>
       </article>
