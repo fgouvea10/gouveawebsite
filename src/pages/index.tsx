b213fe204531296aa8d81b3d5e5a9d02b7bf5de6
Felipe Gouvea <felipegalvesouvea@gmail.com>
2022-03-14 20:37:03 -0300

    feature: create hero in home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
 
 import Head from 'next/head';
+import Link from 'next/link';
+
+import styles from '~/styles/home.module.scss';
 
 export default function Home() {
   return (
@@ -15,7 +18,58 @@ export default function Home() {
         />
       </Head>
 
-      <div></div>
+      <main>
+        <section id={styles.hero}>
+          <div className={styles.container}>
+            <div className={styles.flex}>
+              <div className={styles.heroTexts}>
+                <h1>Uniting experience and passion with a single purpose</h1>
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                  do eiusmod tempor incididunt ut labore et dolore magna aliqua
+                </p>
+              </div>
+
+              <button type="button" aria-label="Contact me">
+                go
+              </button>
+            </div>
+
+            <div className={styles.works}>
+              <h3>Works that I{"'"}m proud to show</h3>
+              <div className={styles.cards}>
+                <div className={styles.card}>
+                  <div className={styles.cardContent}>
+                    <h5>ignews</h5>
+                    <span>Web Development</span>
+                    <Link href="/">See study case</Link>
+                  </div>
+                </div>
+                <div className={styles.card}>
+                  <div className={`${styles.cardContent} ${styles.active}`}>
+                    <h5>ignews</h5>
+                    <span>Web Development</span>
+                    <Link href="/">See study case</Link>
+                  </div>
+                  <div className={styles.featuredWork}>NE</div>
+                </div>
+                <div className={styles.card}>
+                  <div className={styles.cardContent}>
+                    <h5>ignews</h5>
+                    <span>Web Development</span>
+                    <Link href="/">See study case</Link>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div className={styles.copy}>
+              <div />
+              <span>&copy; 2022</span>
+            </div>
+          </div>
+        </section>
+      </main>
     </>
   );
 }
