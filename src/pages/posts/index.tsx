df69c1380b52c8bf86e3bec3df4226f7b5302913
Felipe Gouvea <felipegalvesouvea@gmail.com>
2022-03-18 20:57:29 -0300

    feature: create loading component

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -1,12 +1,21 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 import Head from 'next/head';
 
 import BlogCard from '~/components/Cards/Blog';
+import BlogCardLoading from '~/components/Cards/Blog/loadingCard';
 import Header from '~/components/Header';
 import styles from '~/styles/posts.module.scss';
 
 export default function Posts() {
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 3000);
+  }, []);
+
   return (
     <>
       <Head>
@@ -18,9 +27,13 @@ export default function Posts() {
         <section id={styles.posts}>
           <div className={styles.container}>
             <div className={styles.cards_container}>
-              {Array.from({ length: 7 }, (_, index) => (
-                <BlogCard key={index} />
-              ))}
+              {(isLoading &&
+                Array.from({ length: 3 }, (_, index) => (
+                  <BlogCardLoading key={index} />
+                ))) ||
+                Array.from({ length: 7 }, (_, index) => (
+                  <BlogCard key={index} />
+                ))}
             </div>
           </div>
         </section>
