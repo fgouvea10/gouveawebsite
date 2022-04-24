9cbb5b4d361e4187ce442390111e58be0317062f
Felipe Gouvea <felipegalvesouvea@gmail.com>
2022-04-24 18:30:39 -0300

    feat: add new landing page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 
 import Head from 'next/head';
 
+import styles from '~/styles/home.module.scss';
+
 export default function Home() {
   return (
     <>
@@ -16,9 +18,39 @@ export default function Home() {
       </Head>
 
       <main>
-        {Array.from({ length: 190 }, () => (
-          <div>olá</div>
-        ))}
+        <section className={`${styles.landing} section`}>
+          <div className="container">
+            <div className={styles.texts}>
+              <h1>
+                I believe that
+                <br />
+              </h1>
+              <h1>
+                development can
+                <br />
+              </h1>
+              <h1>
+                solve problems
+                <br />
+              </h1>
+            </div>
+
+            <div className={styles.bio}>
+              I bring together science and art to create a <b>brand</b> and{' '}
+              <b>website</b> projects that makes difference in your business
+            </div>
+
+            <div className={styles['button-container']}>
+              <button type="button" className="primary">
+                check out my porfolio
+              </button>
+
+              <button type="button" className="secondary">
+                say hello
+              </button>
+            </div>
+          </div>
+        </section>
       </main>
     </>
   );
