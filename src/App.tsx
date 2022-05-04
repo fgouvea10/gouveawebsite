ff57766501489176431e030c5005f29e1e0af931
fgouvea10 <felipegalvesouvea@gmail.com>
2022-05-03 22:21:34 -0300

    refactor: back to nextjs and adding tailwindcss

--- src/App.tsx
+++
@@ -1,9 +0,0 @@
-function App() {
-  return (
-    <div className="w-screen h-screen flex items-center justify-center">
-      <h1>Comming soon...</h1>
-    </div>
-  );
-}
-
-export default App;
---
+++ src/pages/index.tsx
@@ -0,0 +1,17 @@
+import type { NextPage } from "next";
+
+import Head from "next/head";
+
+export default function App() {
+  return (
+    <>
+      <Head>
+        <title>Felipe Gouvea</title>
+      </Head>
+
+      <main className="w-screen h-screen flex items-center justify-center bg-neutral-900">
+        <h1 className="text-neutral-200">Comming soon...</h1>
+      </main>
+    </>
+  );
+}
