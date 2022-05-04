11f7b097488a5688d95102de24668ec7a7ef0afb
Felipe Gouvea <felipegalvesouvea@gmail.com>
2022-05-04 15:26:14 -0300

    feat: update text for view

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -10,7 +10,7 @@ export default function App() {
       </Head>
 
       <main className="w-screen h-screen flex items-center justify-center bg-neutral-900">
-        <h1 className="text-neutral-200">Comming soon...</h1>
+        <h1 className="text-neutral-200">New version comming soon...</h1>
       </main>
     </>
   );
