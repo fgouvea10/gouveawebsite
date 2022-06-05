4d7bebd6ca4e75034577d62818f34178ff0bb12d
Felipe Gouvea <felipegalvesouvea@gmail.com>
2022-06-05 20:47:55 -0300

    feat: add react query, create useFetch hook and add github repositories

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,9 @@
+import { QueryClientProvider } from 'react-query';
+
 import { useRouter } from 'next/router';
 import type { AppProps } from 'next/app';
 
+import { client } from 'services/queryClient';
 import { Header, Footer, AdminHeader } from 'components/layout';
 
 import 'styles/global.css';
@@ -18,28 +21,28 @@ function MyApp({ Component, pageProps }: AppProps) {
 
   if (router.pathname === '/sign-in') {
     return (
-      <>
+      <QueryClientProvider client={client}>
         <AdminHeader shouldDisplayUserInfo={false} />
         <Component {...pageProps} />
-      </>
+      </QueryClientProvider>
     );
   }
 
   if (shouldDisplayUserInfo.includes(router.pathname)) {
     return (
-      <>
+      <QueryClientProvider client={client}>
         <AdminHeader shouldDisplayUserInfo />
         <Component {...pageProps} />
-      </>
+      </QueryClientProvider>
     );
   }
 
   return (
-    <>
+    <QueryClientProvider client={client}>
       <Header />
       <Component {...pageProps} />
       <Footer />
-    </>
+    </QueryClientProvider>
   );
 }
 
--- src/pages/works.tsx
+++ src/pages/works.tsx
@@ -5,8 +5,25 @@ import type { NextPage } from 'next';
 import { DefaultCard } from 'components/shared/Cards/Default';
 
 import styles from 'styles/modules/Works.module.css';
+import { useFetch } from 'hooks/fetch';
+import axios from 'axios';
+import { useEffect } from 'react';
+import { useQuery } from 'react-query';
+import { IRepositoriesDTO } from 'dtos/IRepositoriesDTO';
+import { formatDate } from 'utils/formatDate';
 
 const Works: NextPage = () => {
+  const { data: repositories } = useFetch<IRepositoriesDTO[]>(
+    'repositories',
+    async () => {
+      const response = await axios.get(
+        'https://api.github.com/users/fgouvea10/repos'
+      );
+
+      return response.data;
+    }
+  );
+
   const works = [
     {
       id: 1,
@@ -24,7 +41,7 @@ const Works: NextPage = () => {
       <main className={styles.main}>
         <section className={styles.works}>
           <div className={styles.container}>
-            <h1>Recent works</h1>
+            <h2>Recent works</h2>
             <div className={styles.cards}>
               {works.map((work, index) => (
                 <DefaultCard
@@ -32,6 +49,38 @@ const Works: NextPage = () => {
                   data={work}
                   element={<Link href="/">see study case</Link>}
                 />
+              ))}
+            </div>
+          </div>
+        </section>
+
+        <section className={styles['git-repositories']}>
+          <div className={styles.container}>
+            <h2>Git repositories</h2>
+            <div className={styles['git-cards']}>
+              {repositories?.map((repository: IRepositoriesDTO) => (
+                <Link key={repository.id} href={repository.html_url}>
+                  <div className={styles['git-card']}>
+                    <div className={styles['git-card-header']}>
+                      <div className={styles['git-card-content']}>
+                        <time>{formatDate(repository.created_at)}</time>
+                      </div>
+                      <div className={styles.divider} />
+                    </div>
+                    <div className={styles['git-card-body']}>
+                      <div className={styles['git-card-content']}>
+                        <div className={styles.picture}>
+                          <img
+                            src="/images/git.png"
+                            alt="Drawing of a cat above the water"
+                          />
+                        </div>
+                        <strong>{repository.name}</strong>
+                        <p>{repository.description ?? '-'}</p>
+                      </div>
+                    </div>
+                  </div>
+                </Link>
               ))}
             </div>
           </div>
