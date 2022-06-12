88905a324c0df16bef065ee8c0c81c6c163a4a03
Felipe Gouvea <felipegalvesouvea@gmail.com>
2022-06-12 00:27:54 -0300

    feat: add update work controller/use case

---
+++ backend/src/modules/works/use-cases/update-work/update-work-controller.ts
@@ -0,0 +1,20 @@
+import { Request, Response } from 'express';
+
+import { UpdateWorkUseCase } from './update-work-use-case';
+
+export class UpdateWorkController {
+  async handle(request: Request, response: Response) {
+    const { id } = request.params;
+    const { name, excerpt, content } = request.body;
+
+    const updateWorkUseCase = new UpdateWorkUseCase();
+    const result = await updateWorkUseCase.execute({
+      id,
+      name,
+      excerpt,
+      content,
+    });
+
+    return response.json(result);
+  }
+}
