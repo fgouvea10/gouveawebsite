317b4f2dc6fce8c6224c6d2ea0d20b9344908258
Felipe Gouvea <felipegalvesouvea@gmail.com>
2022-06-14 21:41:02 -0300

    feat: add delete user use-case/controller

--- backend/src/shared/infra/http/routes/user.routes.ts
+++ backend/src/shared/infra/http/routes/user.routes.ts
@@ -2,6 +2,7 @@ import { Router } from "express";
 
 import { AuthenticateUserController } from "../../../../modules/account/authenticate-user/authenticate-user-controller";
 import { CreateUserController } from "../../../../modules/users/use-cases/create-user/create-user-controller";
+import { DeleteUserController } from "../../../../modules/users/use-cases/delete-user/delete-user-controller";
 import { ListUsersController } from "../../../../modules/users/use-cases/list-users/list-users-controller";
 
 import { ensureAuthenticated } from "../middlewares/ensure-authenticated";
@@ -11,7 +12,9 @@ export const userRoutes = Router();
 const createUserController = new CreateUserController();
 const authenticateUserController = new AuthenticateUserController();
 const listUsersController = new ListUsersController();
+const deleteUserController = new DeleteUserController();
 
 userRoutes.post("/create", createUserController.handle);
 userRoutes.post("/session", authenticateUserController.handle);
 userRoutes.get("", ensureAuthenticated, listUsersController.handle);
+userRoutes.get("/:id", ensureAuthenticated, deleteUserController.handle);
