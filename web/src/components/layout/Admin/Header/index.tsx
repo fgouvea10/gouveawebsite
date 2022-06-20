c6961842a070bfbc4bab87998a4ad9dd3a1b0217
Felipe Gouvea <felipegalvesouvea@gmail.com>
2022-06-20 11:12:43 -0300

    feat: add sidebar responsiveness

--- web/src/components/layout/Admin/Header/index.tsx
+++ web/src/components/layout/Admin/Header/index.tsx
@@ -26,10 +26,15 @@ export function AdminHeader({
   shouldDisplayUserInfo = false,
 }: AdminHeaderProps) {
   const [user, setUser] = useState<User>({} as User);
+  const [isMenuActive, setIsMenuActive] = useState(false);
   const headerRef = useRef(null);
 
   const router = useRouter();
   const { signOut } = useAuth();
+
+  const classMenu = isMenuActive ? `${styles['active']}` : '';
+  const classButtons = isMenuActive ? `${styles['active']}` : '';
+  const classContent = isMenuActive ? 'fade-in' : 'fade-out';
 
   const { logoTextWhite: gouveaLogo } = logoConfig;
 
@@ -67,15 +72,48 @@ export function AdminHeader({
 
   return (
     <header ref={headerRef} className={styles.header}>
-      <Link href="/">
-        <Image
-          src={gouveaLogo}
-          alt="gouvea logotype"
-          width={107}
-          height={22}
-          style={{ zIndex: 6, cursor: 'pointer' }}
-        />
-      </Link>
+      <div className={styles['header-left']}>
+        <button
+          className={`${styles.menutrigger} ${classButtons}`}
+          onClick={() => setIsMenuActive(!isMenuActive)}
+          type="button"
+          aria-label="Toggle menu"
+        >
+          <div className={styles.bar} />
+          <div className={styles.bar} />
+        </button>
+        <Link href="/">
+          <Image
+            src={gouveaLogo}
+            alt="gouvea logotype"
+            width={107}
+            height={22}
+            style={{ zIndex: 6, cursor: 'pointer' }}
+          />
+        </Link>
+      </div>
+
+      <div id={styles.menu} className={classMenu}>
+        <nav className={`${styles.items} ${classContent}`}>
+          <ul>
+            {SIDEBAR_MOCK.map((item, index) => (
+              <li
+                key={String(index + 1)}
+                className={
+                  router.pathname === item.path
+                    ? `${styles['active-link']}`
+                    : ''
+                }
+                onClick={() => setIsMenuActive(!isMenuActive)}
+              >
+                <Link key={String(index + 1)} href={item.path} target="_blank">
+                  {item.name}
+                </Link>
+              </li>
+            ))}
+          </ul>
+        </nav>
+      </div>
 
       {shouldDisplayUserInfo && (
         <div className={styles['user-info']}>
@@ -99,3 +137,18 @@ export function AdminHeader({
     </header>
   );
 }
+
+const SIDEBAR_MOCK = [
+  {
+    name: 'dashboard',
+    path: '/admin',
+  },
+  {
+    name: 'works',
+    path: '/admin/works',
+  },
+  {
+    name: 'posts',
+    path: '/admin/posts',
+  },
+];
