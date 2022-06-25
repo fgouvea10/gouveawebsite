4f65c7fac639e5bf58f1a9e0717e0f9ef0aea178
Felipe Gouvea <felipegalvesouvea@gmail.com>
2022-06-25 19:15:06 -0300

    feat: remove backend and monorepo

--- backend/src/modules/users/use-cases/list-users/list-users-use-case.ts
+++
@@ -1,9 +0,0 @@
-import { prisma } from "../../../../shared/infra/prisma";
-
-export class ListUsersUseCase {
-  async execute() {
-    const users = await prisma.users.findMany();
-
-    return users;
-  }
-}
