bce382a3997120b096ec35d76f270d506fc0b2df
Felipe Gouvea <felipegalvesouvea@gmail.com>
2022-06-30 15:02:27 -0300

    feat: add global style and custom font

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
+import { Home } from "./presentation/pages/Home";
+
+import './presentation/styles/global.scss';
+
 export default function App() {
-  return (
-    <div><div></div></div>
-  )
+  return <Home />;
 }
