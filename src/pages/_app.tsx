c8a9a085979f627f88f77f4860028833038c2e5c
Felipe Gouvea <felipegalvesouvea@gmail.com>
2022-07-02 00:04:02 -0300

    feat: create header component

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,16 @@
+import { Fragment } from 'react';
+import { Header } from 'components/shared/Header';
 import type { AppProps } from 'next/app';
 
 import 'styles/global.scss';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <Fragment>
+      <Header />
+      <Component {...pageProps} />
+    </Fragment>
+  );
 }
 
 export default MyApp;
